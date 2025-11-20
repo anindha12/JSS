cfa669e7b8be295517823dda8b425833cdc6af8f
anindha12 <anusky.anu@gmail.com>
2025-11-20 19:52:03 +0600

    commit

--- script.js
+++ script.js
@@ -6,24 +6,29 @@ var perimeter;
 
 if (shape === "square") {
     var side = Number(prompt("Enter the side length:"));
-    perimeter = 4 * side;
+    var h = 4 * side;
+    alert("The perimeter of the square is: " + h);
+
 }
 
 if (shape === "rectangle") {
     var length = Number(prompt("Enter the length:"));
     var width = Number(prompt("Enter the width:"));
-    perimeter = 2 * (length + width);
+    var f = 2 * (length + width);
+    alert("The perimeter of the rectangle is: " + f);
 }
 
 if (shape === "triangle") {
     var a = Number(prompt("Enter side A:"));
     var b = Number(prompt("Enter side B:"));
     var c = Number(prompt("Enter side C:"));
-    perimeter = a + b + c;
+    var e = a + b + c;
+    alert("The perimeter of the triangle is: " + e);
 }
 
 if (shape === "circle") {
     var r = Number(prompt("Enter the radius:"));
-    perimeter = 2 * Math.PI * r;
+   var v = 2 * Math.PI * r
+   alert("The perimeter (circumference) of the circle is: " + v);
 }
 
